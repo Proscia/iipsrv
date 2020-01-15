006724b3569ed6c472bad412daf93ea0e1e9d01a
Leo Laroco <leo@proscia.com>
2020-01-15 15:30:17 -0500

    CZIImage::loadImageInfo() stuff -- Stumbling through adding Zeiss CZI to IIPSRV.

--- src/CZIImage.cc
+++ src/CZIImage.cc
@@ -26,6 +26,8 @@
 #include <iostream>
 #include <fstream>
 
+#include "CziUtils.h"
+
 extern std::ofstream logfile;
 
 using namespace std;
@@ -62,7 +64,7 @@ void CZIImage::openImage()
 void CZIImage::closeImage()
 {
   if( czi_reader != NULL ){
-	czi_reader->Close();
+    czi_reader->Close();
     czi_reader = NULL;
   }
   if( tile_buf != NULL ){
@@ -84,54 +86,29 @@ void CZIImage::loadImageInfo( int x, int y )
   auto sbStatistics = czi_reader->GetStatistics();
 
   // Store the list of image dimensions available
+
+  /// The image pixel dimensions
+  ///   std::vector <unsigned int> image_widths, image_heights;
+  /// The number of available resolutions in this image
+  ///   unsigned int numResolutions;
+
   // See:  CZICmd/execute.cpp -- PrintStatistics().
-  ////unsigned int full_width = sbStatistics.boundingBox.w;
-  ////unsigned int full_height = sbStatistics.boundingBox.h;
-  unsigned int full_width = sbStatistics.boundingBoxLayer0Only.w;
-  unsigned int full_height = sbStatistics.boundingBoxLayer0Only.h;
+  /*
+Bounding-Box:
+ All:    X=0 Y=0 W=98172 H=66096
+ Layer0: X=0 Y=0 W=98097 H=65963
+  */
+  unsigned int full_width = sbStatistics.boundingBox.w;
+  unsigned int full_height = sbStatistics.boundingBox.h;
+  ////unsigned int full_width = sbStatistics.boundingBoxLayer0Only.w;
+  ////unsigned int full_height = sbStatistics.boundingBoxLayer0Only.h;
   image_widths.push_back( full_width );
   image_heights.push_back( full_height );
   numResolutions = 1;
 
   // CZI pyramid layers as virtual width x height images.
   // See:  CZICmd/execute.cpp -- PrintPyramidStatistics().
-  auto pyrStatistics = czi_reader->GetPyramidStatistics();
-  auto scene0PyrStatistics = pyrStatistics.scenePyramidStatistics[0];
-  for (const auto& j : scene0PyrStatistics) {
-    if (!j.layerInfo.IsNotIdentifiedAsPyramidLayer()) {
-      if (j.layerInfo.IsLayer0() != true) {
-        int scale = j.layerInfo.minificationFactor;
-        for (int n = 1; n < j.layerInfo.pyramidLayerNo; ++n) {
-          scale *= j.layerInfo.minificationFactor;
-        }
-
-		// Scaled down dimensions [rounded up with (.. -1)/scale +1]
-		unsigned int w = (full_width -1)/scale +1;
-		unsigned int h = (full_height -1)/scale +1;
-		// Ignore downsamples smaller than 2K x 2K.  TODO(Leo) Why?  Ask Coleman.
-		if (w < 2000 && h < 2000) {
-		  break;
-		}
-
-		image_widths.push_back( w );
-		image_heights.push_back( h );
-		++numResolutions;
-      }
-    }
-  }
-#if 0  // TODO(Leo)  IIPImage metadata to be initialized.
-  /// The image pixel dimensions
-  std::vector <unsigned int> image_widths, image_heights;
   /*
-	static void PrintStatistics(const CCmdLineOptions& options, ICZIReader* reader)
-		WriteIntRect(ss, sbStatistics.boundingBox);
-
-
-18_WT1_NKPP002_01_R_LS.czi.PrintInformation.txt
-Bounding-Box:
- All:    X=0 Y=0 W=98172 H=66096
- Layer0: X=0 Y=0 W=98097 H=65963
-
 scene#0:
  number of subblocks with scale 1/1: 2550
  number of subblocks with scale 1/3: 919
@@ -139,132 +116,129 @@ scene#0:
  number of subblocks with scale 1/27: 17
  number of subblocks with scale 1/81: 3
  number of subblocks with scale 1/243: 1
-
->>> 98172/243.
-404.0
->>> 66096/243.
-272.0
-
-
-GR57-13 2015_10_12__0078.czi.PrintInformation.txt
-Bounding-Box:
- All:    X=-134078 Y=9180 W=119630 H=48000
- Layer0: X=-134078 Y=9180 W=119625 H=47998
-
-scene#0:
- number of subblocks with scale 1/1: 1311
- number of subblocks with scale 1/2: 1221
- number of subblocks with scale 1/4: 333
- number of subblocks with scale 1/8: 99
- number of subblocks with scale 1/16: 30
- number of subblocks with scale 1/32: 12
- number of subblocks with scale 1/64: 3
-
-scene#1:
- number of subblocks with scale 1/1: 1182
- number of subblocks with spcale 1/2: 1110
- number of subblocks with scale 1/4: 303
- number of subblocks with scale 1/8: 90
- number of subblocks with scale 1/16: 33
- number of subblocks with scale 1/32: 12
- number of subblocks with scale 1/64: 3
-
->>> 119630/64
-1869.21875
->>> 48000/64
-750.0
->>> 119630/4 
-29907.5
-
-
-1_CD3645_Ins594_Glu488_DAPI_IS4612.czi.PrintInformation.txt
-Bounding-Box:
- All:    X=-111996 Y=34869 W=5731 H=7580
- Layer0: X=-111996 Y=34869 W=5731 H=7580
-
-scene#0:
- number of subblocks with scale 1/1: 480
- number of subblocks with scale 1/2: 480
- number of subblocks with scale 1/4: 160
- number of subblocks with scale 1/8: 40
-
->>> 7580/8
-947.5
->>> 5731/8
-716.375
->>> 7580/4
-1895.0
->>> 5731/4
-1432.75
   */
-#endif // TODO(Leo)  IIPImage metadata to be initialized.
+  auto pyrStatistics = czi_reader->GetPyramidStatistics();
+  auto scene0PyrStatistics = pyrStatistics.scenePyramidStatistics[0];
+  for (const auto& layer_stats : scene0PyrStatistics) {
+    if (!layer_stats.layerInfo.IsNotIdentifiedAsPyramidLayer()) {
+      if (layer_stats.layerInfo.IsLayer0() != true) {
+        int scale = layer_stats.layerInfo.minificationFactor;
+        for (int n = 1; n < layer_stats.layerInfo.pyramidLayerNo; ++n) {
+          scale *= layer_stats.layerInfo.minificationFactor;
+        }
 
-  // CZI images are not stored as grids of tiles;
-  // virtual tiles will be created on the fly.
-  tile_width = 1024;
-  tile_height = 1024;
-#if 0  // TODO(Leo)  IIPImage metadata to be initialized.
+        // Scaled down dimensions [rounded up with (.. -1)/scale +1]
+        unsigned int w = (full_width -1)/scale +1;
+        unsigned int h = (full_height -1)/scale +1;
+        // Ignore downsamples smaller than 2K x 2K.
+        // TODO(Leo) Why?  Ask Coleman.  Maybe don't care about viewing anything smaller.
+        if (w < 2000 && h < 2000) {
+          break;
+        }
+
+        image_widths.push_back( w );
+        image_heights.push_back( h );
+        ++numResolutions;
+      }
+    }
+  }
+
+
   /// The base tile pixel dimensions
-  unsigned int tile_width, tile_height;
+  ///   unsigned int tile_width, tile_height;
+  // CZI images are not stored as grids of tiles,
+  // so output a default virtual tile dimensions, e.g.: 728x728 or 1024x1024
+  // Virtual tiles will be created on the fly.
+  tile_width = 1024;   // TODO(Leo) Make 1024 a macro or constant in CZIImage.h
+  tile_height = 1024;
+
+  // See:  CZICmd/execute.cpp -- PrintAllSubBlocks().
   /*
-CZI aren't arranged as tiles, so output a default virtual tile dimensions, e.g.: 728x728 or 1024x1024
-  */
-
-#endif // TODO(Leo)  IIPImage metadata to be initialized.
-
-#if 0  // TODO(Leo)  IIPImage metadata to be initialized.
-  /// The colour space of the image
-  ColourSpaces colourspace;
-  /*
-execute.cpp:: static void PrintDisplaySettingsForChannel(int ch, IChannelDisplaySetting* dsplChSettings, const CCmdLineOptions& options)
-
 Complete list of sub-blocks
 ---------------------------
 
 #0: C0S0B0 M=0 logical=(0,0,98172,66096) phys.=(404,272) pixeltype=bgr24 comp.mode=jpgxr
-
-
-$ grep -Po 'pixeltype=\S+|^ C -> Start=\d+ Size=\d+' -r | sort | uniq -c
-      1 18_WT1_NKPP002_01_R_LS.czi.PrintInformation.txt: C -> Start=0 Size=1
-   3606 18_WT1_NKPP002_01_R_LS.czi.PrintInformation.txt:pixeltype=bgr24
-      1 1_CD3645_Ins594_Glu488_DAPI_IS4612.czi.PrintInformation.txt: C -> Start=0 Size=4
-   1160 1_CD3645_Ins594_Glu488_DAPI_IS4612.czi.PrintInformation.txt:pixeltype=gray16
   */
+  libCZI::PixelType pixel_type = libCZI::PixelType::Invalid;
+  czi_reader->EnumerateSubBlocks(
+      [&](int index, const libCZI::SubBlockInfo& info)->bool
+  {
+	if (pixel_type == libCZI::PixelType::Invalid)
+	  pixel_type = info.pixelType;
+	else if (pixel_type != info.pixelType)
+	  return false;
 
+	return true;
+  });
 
-#endif // TODO(Leo)  IIPImage metadata to be initialized.
+  /// The colour space of the image
+  ///   ColourSpaces colourspace;
+  switch (pixel_type) {
+    case libCZI::PixelType::Gray8:
+    case libCZI::PixelType::Gray16:
+    case libCZI::PixelType::Gray32:
 
-#if 0  // TODO(Leo)  IIPImage metadata to be initialized.
-  /// The number of available resolutions in this image
-  // CZIcmd --source ../AC5537\ \ insulin\ CD4.czi --command PrintInformation --info-level 'PyramidStatistics'
-  unsigned int numResolutions;  // --command PrintInformation --info-level 'PyramidStatistics'
-  /*
-	static void PrintPyramidStatistics(ICZIReader* reader, const CCmdLineOptions& options)
+    case libCZI::PixelType::Gray32Float:
+    case libCZI::PixelType::Gray64Float:
+    case libCZI::PixelType::Gray64ComplexFloat:
+      colourspace = GREYSCALE;
+	  break;
 
-      1 18_WT1_NKPP002_01_R_LS.czi.PrintInformation.txt: C -> Start=0 Size=1
-  */
+    case libCZI::PixelType::Bgr24:
+    case libCZI::PixelType::Bgr48:
 
-#endif // TODO(Leo)  IIPImage metadata to be initialized.
+    case libCZI::PixelType::Bgra32:
 
-#if 0  // TODO(Leo)  IIPImage metadata to be initialized.
+    case libCZI::PixelType::Bgr96Float:
+    case libCZI::PixelType::Bgr192ComplexFloat:
+	  colourspace = sRGB;
+	  break;
+
+    case libCZI::PixelType::Invalid:
+    default:
+	  colourspace = NONE;
+	  break;
+  }
+
   /// The bits per channel for this image
-  unsigned int bpc;
+  ///   unsigned int bpc;
+  bpc = 8 * CziUtils::GetBytesPerPel(pixel_type);
 
-#endif // TODO(Leo)  IIPImage metadata to be initialized.
-
-#if 0  // TODO(Leo)  IIPImage metadata to be initialized.
   /// The number of channels for this image
-  // CZIcmd --source ../1_CD3645_Ins594_Glu488_DAPI_IS4612.czi --command PrintInformation --info-level 'Statistics'
-  //     C -> Start=0 Size=4
-  unsigned int channels;
+  ///   unsigned int channels;
+  // See:  CZICmd/execute.cpp -- PrintStatistics().
+  int channels_start = 0;
+  int channels_size = 0;  // Unchanged if TryGetInterval() == false.
+  sbStatistics.dimBounds.TryGetInterval(libCZI::DimensionIndex::C, &channels_start, &channels_size);
+  channels = (unsigned int) channels_size;
 
-#endif // TODO(Leo)  IIPImage metadata to be initialized.
-
-#if 0  // TODO(Leo)  IIPImage metadata to be initialized.
   /// The sample format type (fixed or floating point)
-  SampleType sampleType;
+  ///   SampleType sampleType;
+  switch (pixel_type) {
 
-#endif // TODO(Leo)  IIPImage metadata to be initialized.
+    case libCZI::PixelType::Gray32Float:
+    case libCZI::PixelType::Gray64Float:
+    case libCZI::PixelType::Gray64ComplexFloat:
+
+    case libCZI::PixelType::Bgr96Float:
+    case libCZI::PixelType::Bgr192ComplexFloat:
+	  sampleType == FLOATINGPOINT;
+	  break;
+
+    case libCZI::PixelType::Gray8:
+    case libCZI::PixelType::Gray16:
+    case libCZI::PixelType::Gray32:
+
+    case libCZI::PixelType::Bgr24:
+    case libCZI::PixelType::Bgr48:
+
+    case libCZI::PixelType::Bgra32:
+
+    case libCZI::PixelType::Invalid:
+    default:
+	  sampleType = FIXEDPOINT;
+	  break;
+  }
 
 #if 0  // TODO(Leo)  IIPImage metadata to be initialized.
   /// The min and max sample value for each channel
@@ -329,12 +303,6 @@ Compact:
   /// Quality layers
   //  unsigned int quality_layers;  // Not used for CZI.
 
-  /// Indicate whether we have opened and initialised some parameters for this image
-  //  bool isSet;  // Done - CZIImage::openImage()
-
-  /// If we have an image sequence, the current X and Y position
-  //  int currentX, currentY;  // Below
-
   /// Image histogram
   //  std::vector<unsigned int> histogram;  // Not needed here.
 
@@ -357,158 +325,9 @@ Keywords=
 Rating=0
 CreationDate=2015-10-12T14:05:54.4916631-07:00
   */
-
-  /// Image modification timestamp
-  //  time_t timestamp;  // Done - CZIImage::openImage() .. updateTimestamp()
-#endif // TODO(Leo)
-
-#if 0  // TODO(Leo)
-  tdir_t current_dir;
-  int count;
-  uint16 colour, samplesperpixel, bitspersample, sampleformat;
-  double sminvaluearr[4] = {0.0}, smaxvaluearr[4] = {0.0};
-  double *sminvalue = NULL, *smaxvalue = NULL;
-  unsigned int w, h;
-  string filename;
-  char *tmp = NULL;
-
-  currentX = x;
-  currentY = y;
-
-  // Get the tile and image sizes
-#if 0  // TODO(Leo)
-  // TODO(Leo)
-  /*
-	CZICmd for appropriate metadata???
-	    Extract width and height from CZIcmd Statistics!!!
-CZIcmd --source "${DOWNLOAD}" \
-		   --command PrintInformation --info-level 'Statistics'
-  */
-#endif // TODO(Leo)
-  CZIGetField( czi, CZITAG_TILEWIDTH, &tile_width );
-  CZIGetField( czi, CZITAG_TILELENGTH, &tile_height );
-  CZIGetField( czi, CZITAG_IMAGEWIDTH, &w );
-  CZIGetField( czi, CZITAG_IMAGELENGTH, &h );
-  CZIGetField( czi, CZITAG_SAMPLESPERPIXEL, &samplesperpixel );
-  CZIGetField( czi, CZITAG_BITSPERSAMPLE, &bitspersample );
-  CZIGetField( czi, CZITAG_PHOTOMETRIC, &colour );
-  CZIGetField( czi, CZITAG_SAMPLEFORMAT, &sampleformat );
-
-  if ( samplesperpixel == 1 ){
-    channels = 0;
-
-    unsigned int w1 = w, h1 = h;
-    while ( w1 == w && h1 == h ){
-      CZIReadDirectory( czi );
-      CZIGetField( czi, CZITAG_IMAGEWIDTH, &w1 );
-      CZIGetField( czi, CZITAG_IMAGELENGTH, &h1 );
-      channels++;
-      logfile << "CZIImage:: " << "Dir w " << w1 << " Chan " << channels << endl;
-    }
-  }
-  else {
-    channels = 3;
-  }
-
-  logfile << "CZIImage:: " << "Channels: " << channels << endl;
-
-  bpc = (unsigned int) bitspersample;
-  sampleType = (sampleformat==3) ? FLOATINGPOINT : FIXEDPOINT;
-
-  // // Check for the no. of resolutions in the pyramidal image
-  // current_dir = CZICurrentDirectory( czi );
-  // CZISetDirectory( czi, 0 );
-
-  // Store the list of image dimensions available
-  image_widths.push_back( w );
-  image_heights.push_back( h );
-
-  for( count = 0; CZIReadDirectory( czi ); count++ ){
-    CZIGetField( czi, CZITAG_IMAGEWIDTH, &w );
-    CZIGetField( czi, CZITAG_IMAGELENGTH, &h );
-    // ignore downsamples smaller than 2K x 2K
-    if (w < 2000 && h < 2000) {
-      break;
-    }
-    image_widths.push_back( w );
-    image_heights.push_back( h );
-    current_dir = CZICurrentDirectory( czi );
-    CZISetDirectory( czi, current_dir + channels );
-    logfile << "CZIImage:: " << "Channels: " << count << " w " << w << endl;
-  }
-  // Reset the CZI directory
-  CZISetDirectory( czi, current_dir );
-
-  numResolutions = count+1;
-
-  // Handle various colour spaces
-  if( colour == PHOTOMETRIC_CIELAB ) colourspace = CIELAB;
-  else if( colour == PHOTOMETRIC_MINISBLACK ){
-    colourspace = (bpc==1)? BINARY : GREYSCALE;
-  }
-  else if( colour == PHOTOMETRIC_PALETTE ){
-    // Watch out for colourmapped images. These are stored as 1 sample per pixel,
-    // but are decoded to 3 channels by libczi, so declare them as sRGB
-    colourspace = sRGB;
-    channels = 3;
-  }
-  else if( colour == PHOTOMETRIC_YCBCR ){
-    // JPEG encoded tiles can be subsampled YCbCr encoded. Ask to decode these to RGB
-    CZISetField( czi, CZITAG_JPEGCOLORMODE, JPEGCOLORMODE_RGB );
-    colourspace = sRGB;
-  }
-  else colourspace = sRGB;
-
-  // Get the max and min values for our data type - required for floats
-  // This are usually single values per image, but can also be per channel
-  // in libczi > 4.0.2 via http://www.asmail.be/msg0055458208.html
-
-#ifdef CZITAG_PERSAMPLE
-  if( channels > 1 ){
-    CZISetField(czi, CZITAG_PERSAMPLE, PERSAMPLE_MULTI);
-    CZIGetFieldDefaulted( czi, CZITAG_SMINSAMPLEVALUE, &sminvalue );
-    CZIGetFieldDefaulted( czi, CZITAG_SMAXSAMPLEVALUE, &smaxvalue );
-    CZISetField(czi, CZITAG_PERSAMPLE, PERSAMPLE_MERGED);
-    if (!sminvalue) sminvalue = sminvaluearr;
-    if (!smaxvalue) smaxvalue = smaxvaluearr;
-  }
-  else{
-#endif
-    sminvalue = sminvaluearr;
-    smaxvalue = smaxvaluearr;
-    CZIGetFieldDefaulted( czi, CZITAG_SMINSAMPLEVALUE, sminvalue );
-    CZIGetFieldDefaulted( czi, CZITAG_SMAXSAMPLEVALUE, smaxvalue );
-#ifdef CZITAG_PERSAMPLE
-  }
-#endif
-
-  // Clear our arrays
-  min.clear();
-  max.clear();
-
-  for( unsigned int i=0; i<channels; i++ ){
-    if( (!sminvalue) == smaxvalue[i] ){
-      // Set default values if values not included in header
-      if( bpc <= 8 ) smaxvalue[i] = 255.0;
-      else if( bpc == 12 ) smaxvalue[i] = 4095.0;
-      else if( bpc == 16 ) smaxvalue[i] = 65535.0;
-      else if( bpc == 32 && sampleType == FIXEDPOINT ) smaxvalue[i] = 4294967295.0;
-    }
-    min.push_back( (float)sminvalue[i] );
-    max.push_back( (float)smaxvalue[i] );
-  }
-
-  // Also get some basic metadata
-  if( CZIGetField( czi, CZITAG_ARTIST, &tmp ) ) metadata["author"] = tmp;
-  if( CZIGetField( czi, CZITAG_COPYRIGHT, &tmp ) ) metadata["copyright"] = tmp;
-  if( CZIGetField( czi, CZITAG_DATETIME, &tmp ) ) metadata["create-dtm"] = tmp;
-  if( CZIGetField( czi, CZITAG_IMAGEDESCRIPTION, &tmp ) ) metadata["subject"] = tmp;
-  if( CZIGetField( czi, CZITAG_SOFTWARE, &tmp ) ) metadata["app-name"] = tmp;
-  if( CZIGetField( czi, CZITAG_XMLPACKET, &count, &tmp ) ) metadata["xmp"] = string(tmp,count);
-  if( CZIGetField( czi, CZITAG_ICCPROFILE, &count, &tmp ) ) metadata["icc"] = string(tmp,count);
-
 #endif // TODO(Leo)
 }
+
 
 RawTile CZIImage::getTile( int x, int y, unsigned int res, int layers, unsigned int tile )
 {
